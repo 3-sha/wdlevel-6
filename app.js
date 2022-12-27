936f1680069acaf06c7cecd8f7998f78cb82727d
3-sha <108625495+3-sha@users.noreply.github.com>
2022-12-27 10:24:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
+let csrf = require("tiny-csrf");
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 const path = require("path");
 const { response } = require("express");
+const todo = require("./models/todo");
+let cookieParser = require("cookie-parser");
 //app.js
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname + "/public")));
-// app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser("shh! some secret string"));
+app.use(csrf("this_should_be_32_charactes_long", ["PUT", "POST", "DELETE"]));
 
+app.set("view engine", "ejs");
 app.get("/", async (req, res) => {
   const allTodos = await Todo.getTodos();
   if (req.accepts("html")) {
@@ -47,20 +53,19 @@ app.post("/todos", async (req, res) => {
 });
 
 app.put("/todos/:id/markAsCompleted", async (req, res) => {
-  console.log("Todo marks completed : ", req.params.id);
+  console.log("marks as completed : ", req.params.id);
   const todo = await Todo.findByPk(req.params.id);
   try {
-    const updateTodo = await todo.markAsCompleted();
-    return res.json(updateTodo);
+    const updateTodo = await todo.setCompletionStatus(request.body.completed);
+    return res.json(updatedTodo);
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
   }
 });
-
 // eslint-disable-next-line no-unused-vars
 app.delete("/todos/:id", async (req, res) => {
-  console.log("We have to delete a Todo with ID: ", req.params.id);
+  console.log("deletion a Todo using ID: ", req.params.id);
   const affectedRow = await Todo.destroy({ where: { id: req.params.id } });
   res.send(affectedRow ? true : false);
 });
